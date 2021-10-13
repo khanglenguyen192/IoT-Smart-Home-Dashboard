323da66c1880b32386ee94dcaf19ced069c68a19
Le Nguyen Khang <khang.lenguyen192@gmail.com>
2021-10-14 01:14:12 +0700

    camera function

--- app/src/main/java/com/example/smarthomedashboard/fragment/CameraFragment.java
+++ app/src/main/java/com/example/smarthomedashboard/fragment/CameraFragment.java
@@ -1,21 +1,55 @@
 package com.example.smarthomedashboard.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.smarthomedashboard.R;
 
 public class CameraFragment extends Fragment {
 
+    // Declare
+    private WebView cam1;
+
+    public CameraFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_camera, container, false);
+
+        cam1 = view.findViewById(R.id.cam1);
+
+        String userAgent = System.getProperty( "http.agent" );
+        Log.e("User agent",userAgent);
+
+        cam1.setWebViewClient(new WebViewClient());
+        cam1.getSettings().setUserAgentString(userAgent);
+        cam1.getSettings().setJavaScriptEnabled(true);
+        cam1.getSettings().setAppCacheEnabled(true);
+        cam1.getSettings().setDomStorageEnabled(true);
+        cam1.loadUrl("http://smarthomecamera.ddns.net:8081/");
         return view;
     }
 }
