c0a29fe624ec0b010229c9910702ad6f9d4434a0
baokhanhle123 <baokhanhle27082001@gmail.com>
2021-11-02 15:22:52 +0700

    Implement voice fragment

--- app/src/main/java/com/example/smarthomedashboard/fragment/VoiceFragment.java
+++ app/src/main/java/com/example/smarthomedashboard/fragment/VoiceFragment.java
@@ -1,16 +1,30 @@
 package com.example.smarthomedashboard.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.smarthomedashboard.R;
 
+import java.util.Locale;
+
 public class VoiceFragment extends Fragment {
+
+    TextView textView;
+    ImageButton speaker;
+
+    TextToSpeech tts;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +36,44 @@ public class VoiceFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_voice, container, false);
+
+        textView = (TextView) view.findViewById(R.id.textView);
+        speaker = (ImageButton) view.findViewById(R.id.speaker);
+
+        speaker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Start Speaking");
+                startActivityForResult(intent, 100);
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
+            textView.setText(data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).get(0));
+            //Text to speech
+            speak(textView.getText().toString());
+        }
+    }
+
+    void speak(String txt) {
+        tts = new TextToSpeech(getContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i == TextToSpeech.SUCCESS) {
+                    tts.setLanguage(Locale.US);
+                    tts.setSpeechRate(1.0f);
+                    tts.speak(txt, TextToSpeech.QUEUE_ADD, null);
+                }
+            }
+        });
     }
 }
