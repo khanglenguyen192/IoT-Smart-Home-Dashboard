bb49e403868e4fb1e10b0928e3183a7dfb1c873f
Le Nguyen Khang <khang.lenguyen192@gmail.com>
2021-11-03 04:12:22 +0700

    update Main

--- app/src/main/java/com/example/smarthomedashboard/MainActivity.java
+++ app/src/main/java/com/example/smarthomedashboard/MainActivity.java
@@ -34,7 +34,7 @@ import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static String AIO_key = "aio_TBRn08sLyKXAW4DvhSKkilgWXGI5";
+    public static String AIO_key = "";
 
     // Declare
     private BottomNavigationView main_bottom_navigation;
